b921fd12fdd467018fc41dd359467de94a26a048
Rajendra Rathod <156540009+Rajendra-rathod06@users.noreply.github.com>
2024-02-11 00:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const mobilebtn=document.getElementsByClassName(".menubutton");
+
+// const fornav=document.getElementsByClassName(".nav-link");
+    
+
+// mobilebtn.addEventListener("click",()=>{
+//     fornav.classlist.add('menubutton')
+// });
+
+$(document).ready(function(){
+   let mobilelist =  $(".menubutton").click(function(){
+
+       alert("Please switch to desktop mode for better view website still under development")
+        
+    })
+})
